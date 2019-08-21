f509b2a19fe0d6040d02f0a6e8ae3c01f9a9765a
Aleksandr Falichev <6064233@gmail.com>
2019-08-21 11:50:18 +0800

    Отображение всех элементов списка

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -33,6 +33,17 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_displays_all_list_items(self):
+        """тест: отображаются все элементы списка"""
+
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
 
 class ItemModelTest(TestCase):
     """тест модели списка"""
