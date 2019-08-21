2e50c88b0c586d5aa2624b0539ba508e03404f21
Aleksandr Falichev <6064233@gmail.com>
2019-08-21 11:32:34 +0800

    Редирект главной страницы при пост запросе

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from lists.models import Item
@@ -8,11 +8,7 @@ from lists.models import Item
 def home_page(request):
     """домашняя страница"""
     if request.method == 'POST':
-        new_item_text = request.POST.get('item_text', '')
-        Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
-    return render(request, 'home.html',
-                  {'new_item_text': new_item_text}
-                  )
+    return render(request, 'home.html')
