8e90662c565b2e56826fd1ea50d82550771dddea
Aleksandr Falichev <6064233@gmail.com>
2019-08-21 11:37:08 +0800

    Разделение МТ POST запросов на 2

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -17,11 +17,14 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         """тест: сохранить пост запрос"""
-        # todo: Метод слишком большой, необходимо разбить
-        response = self.client.post('/', data={'item_text': "A new list item"})
+        self.client.post('/', data={'item_text': "A new list item"})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
+        """тест: редирект после пост запроса"""
+        response = self.client.post('/', data={'item_text': "A new list item"})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
 
