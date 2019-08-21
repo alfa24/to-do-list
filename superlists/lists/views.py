94db0e46d7dd60f05196843e326d96d13600819b
Aleksandr Falichev <6064233@gmail.com>
2019-08-21 11:27:33 +0800

    Добавить строку в список только при POST запросе

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,13 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
+from lists.models import Item
 
 
 def home_page(request):
     """домашняя страница"""
+    if request.method == 'POST':
+        new_item_text = request.POST.get('item_text', '')
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
 
     return render(request, 'home.html',
-                  {'new_item_text': request.POST.get('item_text', '')}
+                  {'new_item_text': new_item_text}
                   )
