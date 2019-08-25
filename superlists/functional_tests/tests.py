2e9d6e2ae69823655e06d8e0a57a62aa1b9f86a0
Aleksandr Falichev <6064233@gmail.com>
2019-08-25 18:47:26 +0800

    первые шаги ФТ для макета и стилей

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -110,3 +110,29 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Сделать мушку из павлиньих перьев', page_text)
 
         # Все ложаться спать
+
+    def test_layout_and_styling(self):
+        """тест макета и стиля"""
+
+        # Эдит открывает домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Она замечает что поле ввода аккуратно центрировано
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # она начинает новый список и видит что поле там тоже центрировано
+        inputbox.send_keys('Купить молоко')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молоко')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
