5f0362bf7203519b4a51f090c9afa726cec4c839
Aleksandr Falichev <6064233@gmail.com>
2019-08-27 23:18:46 +0800

    ФТ для проверки невозможности создания пустой записи в списке

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.keys import Keys
+
 from functional_tests.base import FunctionalTest
 
 MAX_WAIT = 10
@@ -11,11 +13,31 @@ class ItemValidationTest(FunctionalTest):
 
         # Эдит открывает домашнюю страницу и случайно пытается отправить
         # пустой элемент списка. Она нажимает Enter на пустом поле ввода
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
         # Домашняя страница обновляется, и появляется сообщение об ошибке,
         # которое говорит, что элементы списка не должны быть пустыми
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "Элементы списка не должны быть пустыми"
+        ))
+
         # Она пробует снова, теперь с неким текстом для элемента, и теперь
         # это срабатывает
+        self.browser.find_element_by_id('id_new_item').send_keys('Купить молока')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молока')
+
         # Как ни странно, Эдит решает отправить второй пустой элемент списка
         # Она получает аналогичное предупреждение на странице списка
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "Элементы списка не должны быть пустыми"
+        ))
+
         # И она может его исправить, заполнив поле неким текстом
-        self.fail('напиши меня!')
+        self.browser.find_element_by_id('id_new_item').send_keys('Купить чая')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молока')
+        self.wait_for_row_in_list_table('2: Купить чая'),
