71bb4821f5c4021814182be644c1c6dd8f2857b5
Aleksandr Falichev <6064233@gmail.com>
2019-08-28 12:34:23 +0800

    Новая форма для элементов списка

---
+++ superlists/lists/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from lists.models import Item
+
+EMPTY_ITEM_ERROR = 'Элементы списка не должны быть пустыми'
+
+
+class ItemForm(forms.ModelForm):
+    """форма для эелемента списка"""
+
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {
+            'text': forms.TextInput(attrs={
+                'placeholder': 'Enter a to-do item',
+                'class': 'form-control input-lg'
+            })
+        }
+        error_messages = {
+            'text': {'required': EMPTY_ITEM_ERROR}
+        }
